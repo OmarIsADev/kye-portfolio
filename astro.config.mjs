2f2b63e6ea2bc897680a963f42312e4c5037ce17
omar saad <omarisadev@gmail.com>
2024-12-28 22:43:24 +0200

    configured config

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,22 +52,22 @@ export default defineConfig({
         access: 'secret'
       }),
 
-      // PUBLIC_VERCEL_ENV: envField.string({
-      //   context: 'client',
-      //   access: 'public',
-      //   optional: true,
-      //   default: 'development'
-      // }),
-      // PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: envField.string({
-      //   context: 'client',
-      //   access: 'public',
-      //   optional: true
-      // }),
-      // PUBLIC_VERCEL_URL: envField.string({
-      //   context: 'client',
-      //   access: 'public',
-      //   optional: true
-      // })
+      PUBLIC_VERCEL_ENV: envField.string({
+        context: 'client',
+        access: 'public',
+        optional: true,
+        default: 'development'
+      }),
+      PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: envField.string({
+        context: 'client',
+        access: 'public',
+        optional: true
+      }),
+      PUBLIC_VERCEL_URL: envField.string({
+        context: 'client',
+        access: 'public',
+        optional: true
+      })
     }
   },
 
